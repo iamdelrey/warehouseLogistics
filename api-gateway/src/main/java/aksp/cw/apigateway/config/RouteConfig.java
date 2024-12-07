9beda6140e933cabd0b0f5560e722bc4a3b5102e
iamdelrey <stepanton1994@mail.ru>
2024-12-07 22:50:30 +0300

    added api gateway

---
+++ api-gateway/src/main/java/aksp/cw/apigateway/config/RouteConfig.java
@@ -0,0 +1,37 @@
+package aksp.cw.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // User Management Service
+                .route("user-service", r -> r.path("/api/users/**")
+                        .uri("http://localhost:8084"))
+
+                // Order Service (GraphQL)
+                .route("order-service", r -> r.path("/api/orders/**")
+                        .filters(f -> f.rewritePath("/api/orders", "/graphql"))
+                        .uri("http://localhost:8082"))
+
+                // Delivery Service
+                .route("delivery-service", r -> r.path("/api/deliveries/**")
+                        .uri("http://localhost:8085"))
+
+                // Inventory Service
+                .route("inventory-service", r -> r.path("/api/products/**")
+                        .uri("http://localhost:8081"))
+
+                // Analytics Service
+                .route("analytics-service", r -> r.path("/api/analytics/**")
+                        .uri("http://localhost:8083"))
+
+                .build();
+    }
+}
